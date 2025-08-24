67aee36e71ce3a632e8d325572b338e411bf74e9
Marcos Ferreto Estrada <marcosferretoestrada@gmail.com>
2025-08-23 20:18:09 -0600

    crear clase jugador

---
+++ main.py
@@ -0,0 +1,8 @@
+class Jugador:
+    def __init__(self, nombre, posicion):
+        self.nombre = nombre
+        self.posicion = posicion
+
+    def mover(self, nueva_posicion):
+        self.posicion = nueva_posicion
+        print(f"{self.nombre} se ha movido a la posición {self.posicion}")
