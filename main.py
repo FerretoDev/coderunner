1bc05cfd2d13bfc279f898c0ad821b5b3f95666c
Marcos Ferreto Estrada <marcosferretoestrada@gmail.com>
2025-08-23 20:19:31 -0600

    cambio de variable a pepe

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class Jugador:
     def __init__(self, nombre, posicion):
         self.nombre = nombre
-        self.posicion = posicion
+        self.posicion = pepe
 
     def mover(self, nueva_posicion):
         self.posicion = nueva_posicion
