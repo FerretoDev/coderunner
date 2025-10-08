69d6f105e7dfe60082393c188e03313eb0b203d4
FerretoDev <marcosferretoestrada@gmail.com>
2025-10-08 14:20:03 -0600

    cambio en hello world

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from game.juego import Juego
 def main():
     juego = Juego()
     juego.iniciar()
-    print('Hello World')
+    print("Hello World, this is a game!")
 
 
 if __name__ == "__main__":
