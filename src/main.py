aa5c6adaab889c917731f0b6982f3536a8d0d4eb
FerretoDev <marcosferretoestrada@gmail.com>
2025-10-16 00:12:38 -0600

    refactor: limpiar importaciones y descomentar lógica de juego en main.py

--- src/main.py
+++ src/main.py
@@ -2,40 +2,39 @@ import sys
 
 import pygame
 
-from game.pruebas_pacman.juego import Juego
+from game.juego import Juego
 
-# from game.juego import Juego
+# from game.pruebas_pacman.juego import Juego
+
 
 # from Prototipo3.juego import Juego
 
 
 def main():
-    # juego = Juego()
-    # juego.iniciar()
+    juego = Juego()
+    juego.iniciar()
 
     # ------
     # from game.pantalla_juego import PantallaJuego
 
     # ----
-
     # Test (please no tocar)
     # pantalla = PantallaJuego("Jugador 1")
     # pantalla.ejecutar()
-
     # También aca
-    try:
-        # Iniciar Pygame
-        pygame.init()
+    # try:
+    # Iniciar Pygame
+    #    pygame.init()
 
-        # crear y correr juego
-        game = Juego()
-        game.run()
-    except Exception as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-    finally:
-        pygame.quit()
-        sys.exit()
+    # crear y correr juego
+    #    game = Juego()
+    #    game.run()
+    # except Exception as e:
+    #    print(f"Error: {e}")
+    #    sys.exit(1)
+    # finally:
+    #    pygame.quit()
+    #    sys.exit()
 
 
 if __name__ == "__main__":
