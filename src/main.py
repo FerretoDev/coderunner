d2dc274f2b252ed1496dfea616d155f27a342430
FerretoDev <marcosferretoestrada@gmail.com>
2025-10-26 21:54:07 -0600

    refactor: limpiar código en main.py eliminando comentarios y código no utilizado

--- src/main.py
+++ src/main.py
@@ -9,27 +9,6 @@ def main():
     juego = Juego()
     juego.iniciar()
 
-    # ------
-    # from game.pantalla_juego import PantallaJuego
-
-    # ----
-    # Test (please no tocar)
-
-    """
-    try:
-        ## Iniciar Pygame
-        pygame.init()nto clas
-
-        # crear y correr juego
-        game = Juego()
-        game.run()
-    except Exception as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-    finally:
-        pygame.quit()
-        sys.exit()"""
-
 
 if __name__ == "__main__":
     main()
