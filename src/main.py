ace3f19b8dade63cf05d69ddfc221bb7127e596c
Marcos Eduardo Ferreto Estrada <marcosferretoestrada@gmail.com>
2025-11-03 02:14:45 -0600

    Add print statement for testing changes

--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ from game.juego import Juego
 def main():
     juego = Juego()
     juego.iniciar()
-
+    print ("Prueba de cambios")
 
 if __name__ == "__main__":
     main()
-print ("Prueba de cambios")
